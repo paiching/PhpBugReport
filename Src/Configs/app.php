1f0695aef5c9e7b1b1216cba17900d87f4317c85
lai <bycheng.lai@gmail.com>
2022-05-30 22:56:39 +0800

    + 資料庫連線測試

--- Src/Configs/app.php
+++ Src/Configs/app.php
@@ -3,7 +3,7 @@
 return [
 
     'app_name' => 'Bug Report App',
-    'env' => 'test',
+    'env' => 'local',
     'debug' => true,
     'log_path' => __DIR__ . '/../Logger',
 ];
--- Src/Configs/database.php
+++ Src/Configs/database.php
@@ -2,4 +2,19 @@
 
 return [
 
+    'pdo' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'db_name' => 'bug_app',
+        'db_username' => 'henry',
+        'db_user_password' => '123',
+        'default_fetch' => PDO::FETCH_OBJ,
+    ],
+    'mysqli' => [
+        'host' => 'localhost',
+        'db_name' => 'bug_app',
+        'db_username' => 'henry',
+        'db_user_password' => '123',
+        'default_fetch' => MYSQLI_ASSOC,
+    ],
 ];
