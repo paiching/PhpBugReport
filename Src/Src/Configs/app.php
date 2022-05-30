81dfd41ef28c98776ee2f0888c0f18c5a9d789c2
lai <bycheng.lai@gmail.com>
2022-05-31 02:47:00 +0800

    +添加Model - 修正提交錯誤 + store.sql 檔案

--- Src/Src/Configs/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-    'app_name' => 'Bug Report App',
-    'env' => 'local',
-    'debug' => true,
-    'log_path' => __DIR__ . '/../Logger',
-];
--- Src/Src/Exception/exception.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-set_error_handler([new \App\Exception\ExceptionHandler(), 'convertWarningsAndNoticesToException']);
-set_exception_handler([new \App\Exception\ExceptionHandler(), 'handle']);
